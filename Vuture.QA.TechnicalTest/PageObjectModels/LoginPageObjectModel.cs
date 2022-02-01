f8accdae4205258f16208358c05442114f2bc25d
charly webster <charly.webster@vutu.re>
2022-02-01 10:35:55 +0000

    Upgraded Packages to Latest versions

--- Vuture.QA.TechnicalTest/PageObjectModels/LoginPageObjectModel.cs
+++ Vuture.QA.TechnicalTest/PageObjectModels/LoginPageObjectModel.cs
@@ -6,8 +6,8 @@ using System.Threading;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Support.PageObjects;
 using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
 using Vuture.QA.TechnicalTest.Driver;
 using Vuture.QA.TechnicalTest.TestBaseStructure;
 
